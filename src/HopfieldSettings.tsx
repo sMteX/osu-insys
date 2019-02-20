4b1185b8fecbe0f343e4291595dc2e6dfda77d5d
Lukáš Stuchlík <stuchlik1@seznam.cz>
2019-02-20 11:30:46 +0100

    Implemented settings

--- src/HopfieldSettings.tsx
+++ src/HopfieldSettings.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import {ISettings, DEFAULT_SETTINGS} from './HopfieldUI';
 
 interface IProps {
-  settings: object;
+  setSettings(newSettings: Partial<ISettings>): any;
 }
 
-export default class HopfieldSettings extends React.Component<IProps, {}> {
+interface IState extends ISettings {}
+
+export default class HopfieldSettings extends React.Component<IProps, IState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      ...DEFAULT_SETTINGS,
+    };
+
+    this.handleSettingsChange = this.handleSettingsChange.bind(this);
+  }
+
+  handleSettingsChange(prop: keyof ISettings) {
+    return (event: React.FormEvent<HTMLInputElement>) => {
+      // @ts-ignore
+      this.setState({
+        [prop]: Number(event.currentTarget.value)
+      });
+      this.props.setSettings({
+        [prop]: Number(event.currentTarget.value),
+      })
+    }
+  }
+
   render(): React.ReactNode {
-    return null;
+    return (
+      <div>
+        <label>dt:</label> <input type="number" step={0.01} onChange={this.handleSettingsChange('dt')} value={this.state.dt} /> <br />
+        <label>alpha:</label> <input type="number" step={1} onChange={this.handleSettingsChange('alpha')} value={this.state.alpha} /> <br />
+        <label>A:</label> <input type="number" step={10} onChange={this.handleSettingsChange('A')} value={this.state.A} /> <br />
+        <label>B:</label> <input type="number" step={10} onChange={this.handleSettingsChange('B')} value={this.state.B} /> <br />
+        <label>C:</label> <input type="number" step={10} onChange={this.handleSettingsChange('C')} value={this.state.C} /> <br />
+        <label>D:</label> <input type="number" step={10} onChange={this.handleSettingsChange('D')} value={this.state.D} /> <br />
+        <label>max iterations:</label> <input type="number" step={10} onChange={this.handleSettingsChange('maxIterations')} value={this.state.maxIterations} />
+      </div>
+    );
   }
 }
--- src/HopfieldUI.tsx
+++ src/HopfieldUI.tsx
@@ -3,19 +3,60 @@ import {Col, Row} from "reactstrap";
 import HopfieldCanvas from "./HopfieldCanvas";
 import HopfieldSettings from "./HopfieldSettings";
 
+export interface ISettings {
+  dt: number;
+  alpha: number;
+  A: number;
+  B: number;
+  C: number;
+  D: number;
+  maxIterations: number;
+}
+
+export const DEFAULT_SETTINGS: ISettings = {
+  dt: 0.05,
+  alpha: 50,
+  A: 500,
+  B: 500,
+  C: 200,
+  D: 500,
+  maxIterations: 1000,
+};
+
 interface IState {
-  settings: object;
+  settings: ISettings;
   cities: any[];
   paths: any[];
 }
 
 export default class HopfieldUI extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      settings: DEFAULT_SETTINGS,
+      cities: [],
+      paths: []
+    };
+
+    this.setSettings = this.setSettings.bind(this);
+  }
+
+  setSettings(newSettings: Partial<ISettings>): void {
+    const merged = {
+      ...this.state.settings,
+      ...newSettings,
+    };
+    this.setState({
+      settings: merged,
+    });
+  }
+
   render(): ReactNode {
     return (
       <Row>
         <Col md={4}>
           Settings
-          <HopfieldSettings settings={this.state.settings} />
+          <HopfieldSettings setSettings={this.setSettings} />
         </Col>
         <Col md={8}>
           Canvas
